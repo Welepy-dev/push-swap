f53c7ff5da574baee3c45c758fc2f0ca11684cc5
welepy-dev <mwsilva.contato@gmail.com>
2025-08-13 18:17:23 +0100

    stack logic

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: marcsilv <marcsilv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 12:18:56 by marcsilv          #+#    #+#             */
-/*   Updated: 2025/08/13 15:35:22 by marcsilv         ###   ########.fr       */
+/*   Updated: 2025/08/13 18:17:15 by marcsilv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,27 @@
 
 typedef struct s_stack
 {
-	int	*array;
+	char	id;
+	int		top;
+	int		size;
+	int		*array;
 }	t_stack;
 
 typedef struct s_ps
 {
-	t_stack	a;
-	t_stack	b;
+	t_stack	*a;
+	t_stack	*b;
 	int		size;
 }	t_ps;
 
 t_ps	*fill_stacks(char **nums);
 char	**check_args(char **nums);
+
+//moves
+void	push(t_stack src, t_stack dst, char *op);
+
+//moves
+bool	is_full(t_stack stack);
+bool	is_empty(t_stack stack);
 
 #endif // !PUSH_SWAP_HPP
