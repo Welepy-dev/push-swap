4428e0c7a0646b4385282fa6a8e881a570943ec0
Welepy <149401955+Welepy-dev@users.noreply.github.com>
2025-08-13 11:48:33 +0000

    code refactor, is ugly but is something for now

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcsilv <marcsilv@42.student.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 12:18:58 by marcsilv          #+#    #+#             */
-/*   Updated: 2025/08/12 19:34:00 by marcsilv         ###   ########.fr       */
+/*   Updated: 2025/08/13 11:42:07 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	main(int ac, char **av)
 
 	if (ac != 1)
 	{
-		if (!(stacks = fill_stacks(av)))
+		if (!(stacks = fill_stacks(check_args(av))))
 			print_error("\0", NULL);
 		clean(stacks);
 	}
