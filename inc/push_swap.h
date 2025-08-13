cb2e5eef082567e6f23ecdcda8b5644cc715f479
welepy-dev <mwsilva.contato@gmail.com>
2025-08-13 11:33:43 +0100

    feat: add argument checking and string validation functions

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -3,17 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcsilv <marcsilv@42.student.fr>          +#+  +:+       +#+        */
+/*   By: marcsilv <marcsilv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 12:18:56 by marcsilv          #+#    #+#             */
-/*   Updated: 2025/08/12 12:19:01 by marcsilv         ###   ########.fr       */
+/*   Updated: 2025/08/13 11:33:36 by marcsilv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include <stdlib.h>
+# include "../libft/libft.h"
+# include <limits.h>
 
 typedef struct s_stack
 {
@@ -25,5 +26,7 @@ typedef struct s_ps
 	t_stack	a;
 	t_stack	b;
 }	t_ps;
+
+bool	check_args(char **av);
 
 #endif // !PUSH_SWAP_HPP
