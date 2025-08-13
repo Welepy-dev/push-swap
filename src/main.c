9c39120c0236d41ca557f744803c7322c3a15f96
welepy-dev <mwsilva.contato@gmail.com>
2025-08-13 19:21:03 +0100

    correct debug functions

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 12:18:58 by marcsilv          #+#    #+#             */
-/*   Updated: 2025/08/13 17:24:22 by marcsilv         ###   ########.fr       */
+/*   Updated: 2025/08/13 19:19:15 by marcsilv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,15 @@ void	clean(t_ps *stacks)
 void	debug_b(t_ps *stacks)
 {
 	ft_printf("STACK B\n");
-	for (int i = 0; i < stacks->b->top; i++)
+	for (int i = stacks->b->top; i >= 0; i--)
 		ft_printf("%d\n", stacks->b->array[i]);
 }
 
 void	debug_a(t_ps *stacks)
 {
 	ft_printf("STACK A\n");
-	for (int i = 0; i < stacks->a->top; i++)
-		ft_printf("%d\n", stacks->a->array[i]);
+	for (int i = stacks->a->top; i >= 0; i--)
+		ft_printf("n%d: %d\n", i, stacks->a->array[i]);
 }
 
 int	main(int ac, char **av)
@@ -47,10 +47,10 @@ int	main(int ac, char **av)
 
 		ft_printf(" == for debug == \n");
 		debug_a(stacks);
-		swap_a(stacks);
+		swap(stacks->a, "sa");
 		debug_a(stacks);
 		ft_printf(" == for debug == \n");
-
+		
 		clean(stacks);
 	}
 	return (0);
